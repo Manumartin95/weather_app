66c928567a7139044e9b992d24279851e84688d4
manuelmartinramos <manuelmartinra@gmail.com>
2025-02-09 20:54:31 +0100

    feat: create use case class

--- lib/features/weather/infrastructure/get_weather_query.dart
+++ lib/features/weather/infrastructure/get_weather_query.dart
@@ -1,10 +1,12 @@
+import 'package:weather_app/core/use-case/use_case.dart';
 import 'package:weather_app/features/weather/domain/weather_repository.dart';
 
-class GetWeatherQuery {
+class GetWeatherQuery implements UseCase<String, dynamic> {
   WeatherRepository repository;
 
   GetWeatherQuery({required this.repository});
 
+  @override
   call(String location) {
     return repository.getWeather(location);
   }
